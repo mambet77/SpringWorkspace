4e522f2529d27dbf8a2e1bb2fa588bf2b41a8ac6
Dimitrij Mackert <dimitrij.mackert@fkie.fraunhofer.de>
2017-02-15 16:53:41 +0100

    ver2

--- RobotSpring_9/src/main/java/ru/javabegin/training/spring/impls/robot/ModelT1000.java
+++ RobotSpring_9/src/main/java/ru/javabegin/training/spring/impls/robot/ModelT1000.java
@@ -7,33 +7,12 @@ import ru.javabegin.training.spring.interfaces.Robot;
 
 public class ModelT1000 implements Robot {
 
-	public Hand getHand() {
-		return hand;
-	}
-
-	public void setHand(Hand hand) {
-		this.hand = hand;
-	}
-
-	public Leg getLeg() {
-		return leg;
-	}
-
-	public void setLeg(Leg leg) {
-		this.leg = leg;
-	}
-
-	public Head getHead() {
-		return head;
-	}
-
-	public void setHead(Head head) {
-		this.head = head;
-	}
-
+	private String color;
 	private Hand hand;
-	private Leg leg;
 	private Head head;
+	private Leg leg;
+	private boolean soundEnabled;
+	private int year;
 
 	public ModelT1000() {
 	}
@@ -45,11 +24,34 @@ public class ModelT1000 implements Robot {
 		this.head = head;
 	}
 
+	public ModelT1000(String color, int year, boolean soundEnabled) {
+		super();
+		this.color = color;
+		this.year = year;
+		this.soundEnabled = soundEnabled;
+	}
+
+	public ModelT1000(String color, Hand hand, Head head, Leg leg, boolean soundEnabled, int year) {
+		super();
+		this.color = color;
+		this.hand = hand;
+		this.head = head;
+		this.leg = leg;
+		this.soundEnabled = soundEnabled;
+		this.year = year;
+	}
+
 	@Override
 	public void action() {
 		head.calc();
 		hand.catchSomething();
 		leg.go();
+		
+		System.out.println("Color: "+ color);
+		System.out.println("Year: "+ year);
+		System.out.println("sound enabled: "+ soundEnabled );
+		
+		
 	}
 
 	@Override
@@ -57,4 +59,51 @@ public class ModelT1000 implements Robot {
 		System.out.println("T1000 is dancing!");
 	}
 
+	public String getColor() {
+		return color;
+	}
+
+	public Hand getHand() {
+		return hand;
+	}
+
+	public Head getHead() {
+		return head;
+	}
+
+	public Leg getLeg() {
+		return leg;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public boolean isSoundEnabled() {
+		return soundEnabled;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public void setHand(Hand hand) {
+		this.hand = hand;
+	}
+
+	public void setHead(Head head) {
+		this.head = head;
+	}
+
+	public void setLeg(Leg leg) {
+		this.leg = leg;
+	}
+
+	public void setSoundEnabled(boolean soundEnabled) {
+		this.soundEnabled = soundEnabled;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
 }
