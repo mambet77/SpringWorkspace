d65aa2d7c6991ca702ca4baec2cc5727359e9b9f
Dimitrij Mackert <dimitrij.mackert@fkie.fraunhofer.de>
2017-03-30 17:26:18 +0200

    s

--- SpringDB_31/SpringDB_31/src/main/java/ru/javabegin/training/spirng/main/Start.java
+++ SpringDB_31/SpringDB_31/src/main/java/ru/javabegin/training/spirng/main/Start.java
@@ -13,14 +13,16 @@ public class Start {
 
 	public static void main(String[] args){
 		MP3 mp3= new MP3();
-		mp3.setAuthor("Song1 author");
-		mp3.setName("Song1 name");
+		mp3.setAuthor("Author author");
+		mp3.setName("Song Sooonh name");
 	
 
 		
 		ApplicationContext context= new ClassPathXmlApplicationContext("context.xml");
 		MP3Dao sqlLiteDao= (SQLiteDAO)context.getBean("sqliteDAO");
-		sqlLiteDao.insert(mp3);
+		//System.out.println(sqlLiteDao.insert(mp3));
+		System.out.println(sqlLiteDao.getMP3Count());
+		System.out.println(sqlLiteDao.groupBy());
 		
 	
 	}
--- SpringDB_31/SpringDB_31/src/main/java/ru/javabegin/training/spring/dao/interfaces/MP3Dao.java
+++ SpringDB_31/SpringDB_31/src/main/java/ru/javabegin/training/spring/dao/interfaces/MP3Dao.java
@@ -1,21 +1,25 @@
 package ru.javabegin.training.spring.dao.interfaces;
 
 import java.util.List;
+import java.util.Map;
 
 import ru.javabegin.training.spring.dao.objects.MP3;
 
 public interface MP3Dao {
 
-	void insert(MP3 mp3);
+	int insert(MP3 mp3);
 
 	void delete(MP3 mp3);
 
 	MP3 getMP3ByID(int id);
 	
 	void delete(int id);
+	
+	int getMP3Count();
 
 	List<MP3> getMP3ListByName(String name);
 
 	List<MP3> getMP3ListByAuthor(String author);
+	Map<String, Integer> groupBy();
 
 }
